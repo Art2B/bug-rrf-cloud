edf45749d92afd291d1384828c3e0d6680778a35
Arthur Battut <arthur.battut@ekino.com>
2018-01-31 16:25:14 +0100

    add firebase with redux and react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, combineReducers, compose } from 'redux'
+import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase'
+import firebase from 'firebase'
+
+import firebaseConfig from './config/firebase'
+
+// initialize firebase instance
+firebase.initializeApp(firebaseConfig)
+
+// Add reactReduxFirebase enhancer when making store creator
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, {
+    userProfile: 'users'
+  }), // firebase instance as first argument
+)(createStore)
+
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer
+})
+
+// Create store with reducers and initial state
+const initialState = {}
+const store = createStoreWithFirebase(rootReducer, initialState)
+
+const App = () => (
+  <Provider store={store}>
+  </Provider>
+)
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
